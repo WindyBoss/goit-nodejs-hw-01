63b3d62936c0b96fe612bab38a57115a94e817bf
Vitaliy Zhyryk <116021952+Vitaliy-Darigo@users.noreply.github.com>
2022-11-14 17:47:51 +0100

    Add Code

---
+++ index.js
@@ -0,0 +1,35 @@
+/** @format */
+const argv = require('yargs').argv;
+
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contact");
+
+async function invokeAction({ action, id, name, email, phone }) {
+    console.log(id);
+    switch (action) {
+      case 'list':
+        console.log(await listContacts());
+        break;
+  
+      case 'get':
+        console.log(await getContactById(id));
+        break;
+  
+      case 'add':
+        addContact(name, email, phone)
+        break;
+  
+      case 'remove':
+        removeContact(id)
+        break;
+  
+      default:
+        console.warn('\x1B[31m Unknown action type!');
+    }
+  }
+  
+  invokeAction(argv);
